79edc4f225cbe9705bf188e9005163d6888d5c63
chessgeek <robert.goodfellow@tufts.edu>
2017-11-12 00:46:22 -0500

    Adds transposition, measure insertion, and metronome manipulation

--- server/ex.py
+++ server/ex.py
@@ -9,6 +9,23 @@ import music21
 # stream to apply the update in, hence the need to
 # pass it as the second argument in all cases.
 
+# Since rests are simply portions of the music where
+# no sound is made, they do not need to be explicitly 
+# notated in the backend representation. Also, 
+# since ties across barlines only depend on a note's 
+# duration, the GUI can figure out where to put 
+# rests and where to put ties without having to 
+# explicitly define them on the backend.
+
+# Though music21 allows explicit definitions of which 
+# 8th/16th notes (and below) should be beamed together
+# into a single unit, this is wildly unnecessary for 
+# our application. Correct beaming would be the job of
+# the GUI, and our GUI may not even address beaming whatsoever.
+# Likewise, for this initial implemenation, articulations
+# expressions, and different clefs besides treble remain 
+# unimplemented. 
+
 def changeKeySig(offset, stream, newKeySig): 
     """ Changes the Key Signature at a given offset inside a stream """
     oldKeySigs = stream.getKeySignatures()
@@ -41,10 +58,10 @@ def renameNotes(startOffset, stream, keySig, endOffset=None):
         offset = note.offset
         if endOffset is None: 
             if startOffset <= offset: 
-                note = renameNote(note, hasSharps)
+                stream.replace(note, renameNote(note, hasSharps))
         else: 
             if startOffset <= offset and offset <= endOffset:
-                note = renameNote(note, hasSharps)
+                stream.replace(note, renameNote(note, hasSharps))
 
 def renameNote(note, hasSharps):       
     """ Rename a note intelligently within a key. """
@@ -79,6 +96,35 @@ def changeTimeSig(offset, stream, newTimeSig):
     stream.insert(offset, newTimeSig)
     # Broadcast
 
+def insertMetronomeMark(offset, stream, mark): 
+    """ Insert a metronome marking in a stream at a given offset. """
+    markings = stream.metronomeMarkBoundaries()
+    for marking in markings: 
+        # Marking already exists at that location, so update it
+        if marking[0] == offset: 
+            marking[2] = mark
+            # Broadcast 
+    stream.insert(offset, mark)
+    # Broadcast
+
+def removeMetronomeMark(offset, stream, mark): 
+    """ Remove a metronome marking in a stream at a given offset. """
+    markings = stream.metronomeMarkBoundaries()
+    for marking in markings: 
+        if marking[0] == offset: 
+            stream.remove(mark)
+            # Broadcast success
+            return
+    # Broadcast failure
+
+def makeMetronomeMark(text, bpm, pulseQLduration, marksMade): 
+    """ Make a metronome marking that can be uniquely identified as 
+        an object during removal if removal is necessary."""
+    mark = music21.tempo.MetronomeMark(text, bpm, pulseQLduration)
+    mark.id = marksMade[0]
+    marksMade[0] += 1
+    return mark
+
 def insertNote(offset, stream, newNote): 
     """ Add a note at a given offset to a stream. """
     notes = stream.notes
@@ -86,12 +132,12 @@ def insertNote(offset, stream, newNote):
         if note.offset == offset: 
             note.add(newNote)
             # Broadcast
-    stream.insert(offset, music21.chord.Chord(newNote.name)) 
+    stream.insert(offset, newNote) 
     # Broadcast
 
 def appendNote(stream, newNote): 
     """ Add a note at the end of a stream. """
-    stream.append(music21.chord.Chord(newNote))
+    stream.append(newNote)
     # Broadcast
 
 def removeNote(offset, stream, removedNoteName): 
@@ -115,6 +161,23 @@ def createNote(pitchName, durationInQLs):
     note.duration = durationInQLs
     chord = music21.chord.Chord(note)
     return chord
+
+def transpose(stream, semitones): 
+    """ Transposes the whole stream up or down 
+        by a number of semitones. """
+    stream.transpose(semitones)
+    # Broadcast whole file
+
+def insertMeasures(insertionOffset, stream, insertedQLs): 
+    """ Insert measures in a stream by moving the offsets of 
+        portions of the score by a given number of QLs. """
+    stream.shiftElements(insertedQLs, insertionOffset)
+    # Broadcast whole file
+
+def sendWholeStream(stream): 
+    """ Send a whole stream of music21 data to a client. """
+    # Broadcast to client
+    stream.offsetMap()
 
 def addLyric(offset, stream, lyric): 
 """ Add lyrics to a given note in the score. """
